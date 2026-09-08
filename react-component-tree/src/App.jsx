e9b5cde21e91ea757c7f7e92c50806f736d356aa
Shrinidhi R <shri.mca.amcec@gmail.com>
2026-09-09 01:19:21 +0530

    Committing to make sure the new change made for the prop concept.

--- react-component-tree/src/App.jsx
+++ react-component-tree/src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header"
 function App() {
   return(
     <>
-      <Header />
+      <Header name = "Components, props, jsx."/>
     </>
   );
 }
--- react-component-tree/src/components/Header.jsx
+++ react-component-tree/src/components/Header.jsx
@@ -1,7 +1,9 @@
-export default function Header(){
+export default function Header(title){
     return(
         <>
         <header>Welcome to my React App ( for components tree )</header>
+        <h3>This is the title of our react concepts for the date 09 / 09 /2026</h3>
+        <h2>concepts name is {title.name}</h2>
         </>
     );
 }
